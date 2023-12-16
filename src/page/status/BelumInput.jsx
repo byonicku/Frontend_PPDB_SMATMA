8bcbc32cf07c67b5a5e0232139d1b7a5c50af18f
Nico Herlim <nicoherlim2003@gmail.com>
2023-12-16 22:47:04 +0700

    status refresh now refresh user too

--- src/page/status/BelumInput.jsx
+++ src/page/status/BelumInput.jsx
@@ -1,12 +1,50 @@
-import StatusComponent from '../../components/success_component/status_component';
+import StatusComponent from "../../components/success_component/status_component";
+import { useEffect } from "react";
+import { useState } from "react";
+import { APIMethod } from "../../utils/ApiMethod";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import getUser from "../../api/UserHandler";
+import { Spinner } from "react-bootstrap";
+import { setUser } from "../../api/UserHandler";
 
 const BelumInput = () => {
-  return (
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const id = JSON.parse(getUser()).data.id_user;
+        const user = await APIMethod.getUserByID(id);
+        delete user.message;
+        setUser(JSON.stringify(user));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        toast.error(error.message);
+        navigate("/");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+
+  return !loading ? (
     <StatusComponent
       title="Anda Belum Input Data!"
       status="failed"
       message="Silahkan input data ke menu berkas terlebih dahulu."
     />
+  ) : (
+    <div className="text-center">
+      <Spinner
+        animation="border"
+        style={{ color: "#0c84a4", width: "3rem", height: "3rem" }}
+      />
+    </div>
   );
 };
 
--- src/page/status/Diterima.jsx
+++ src/page/status/Diterima.jsx
@@ -1,12 +1,50 @@
-import StatusComponent from '../../components/success_component/status_component';
+import StatusComponent from "../../components/success_component/status_component";
+import { useEffect } from "react";
+import { useState } from "react";
+import { APIMethod } from "../../utils/ApiMethod";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import getUser from "../../api/UserHandler";
+import { Spinner } from "react-bootstrap";
+import { setUser } from "../../api/UserHandler";
 
 const Diterima = () => {
-  return (
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const id = JSON.parse(getUser()).data.id_user;
+        const user = await APIMethod.getUserByID(id);
+        delete user.message;
+        setUser(JSON.stringify(user));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        toast.error(error.message);
+        navigate("/");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+
+  return !loading ? (
     <StatusComponent
       title="Selamat!"
       status="success"
       message="Anda dinyatakan lulus dan diterima di SMA Atma Budika!"
     />
+  ) : (
+    <div className="text-center">
+      <Spinner
+        animation="border"
+        style={{ color: "#0c84a4", width: "3rem", height: "3rem" }}
+      />
+    </div>
   );
 };
 
