03183792a116552ef126120f78907247e0360cd6
Nico Herlim <nicoherlim2003@gmail.com>
2023-12-16 19:58:29 +0700

    rapihin api constant dikit

--- src/api/APIConstant.jsx
+++ src/api/APIConstant.jsx
@@ -6,9 +6,18 @@ import axios from "axios";
 // Production
 const BASE_URL = 'tugasbesarpw2.azurewebsites.net';
 
+// Local use HTTP
+// export const API_URL = `http://${BASE_URL}/api`;
+
+// Production use HTTPS
 export const API_URL = `https://${BASE_URL}/api`;
 
+
 export const getPicture = (name) => {
+    // Local Development
+    // return `http://${BASE_URL}/storage/user/${name}`;
+
+    // Production
     return `https://${BASE_URL}/storage/user/${name}`;
   };
 
